866a379a2223726998dbae21620fa054a3316a40
0AntonShevchuk0 <antonshevchuk2002@gmail.com>
2023-11-06 13:53:33 +0200

    synchronize cameras

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,15 +7,16 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : NetworkBehaviour
 {
     [SerializeField] [Range(0f, 100f)] private float speed;
-
+    
     private Rigidbody2D _rigidbody;
+    private CinemachineVirtualCamera _followCamera;
     private Vector2 _direction;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
-    
+
     private void Update()
     {
         if (!IsOwner)
@@ -22,6 +24,15 @@ public class PlayerMovement : NetworkBehaviour
             return;
         }
         MoveToDirection();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            _followCamera = FindObjectOfType<CinemachineVirtualCamera>();
+            _followCamera.Follow = transform;
+        }
     }
     
     private void OnMovement(InputValue value)
